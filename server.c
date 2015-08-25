afae548777a07fc89adf66063fb7248b93d18fe3
praveenast <praveenarmp8@gmail.com>
2015-08-25 15:06:27 +0800

    Test Commit

---
+++ server.c
@@ -0,0 +1,26 @@
+/*
+ * server.c
+ *
+ *  Created on: Jul 9, 2015
+ *      Author: TT112S-04
+ */
+# define MSG_BUF 256
+#include<stdio.h>
+#include<fcntl.h>
+#include<string.h>
+main()
+{
+FILE *file1;
+int fifo_server,fifo_client;
+int choice;
+char buf[MSG_BUF];
+//memset(buf, '\0', sizeof(buf));
+int fd;
+fd = open (" fifo_server" , O_WRONLY );
+/* write into the named - pipe */
+
+strcpy(buf,"Hello");
+write (fd ,buf , strlen(buf));
+printf("11111%s ",buf);
+
+}
